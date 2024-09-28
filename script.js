9310bc790bfe7c1b66cd483aeeb8766006d0779b
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-09-27 17:11:45 -0700

    made the dialing number into a modal instead of an alert and made a number being added to the dnc a modal as well instead of an alert. I made it to where it appends a new p element to the div and will also clear the previous entry each time you add a new number so there arent multip p elements being displayed

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function addToDNC() {
     let phoneNumber = document.getElementById("phoneNumber").value;
     if (phoneNumber) {
         let dncList = JSON.parse(localStorage.getItem('dncList')) || []; // Get existing DNC list or create a new one
+        let newP = document.createElement("p");
+        let modal = document.getElementById("addToDncModal");
         dncList.push(phoneNumber); // Add number to the list
         localStorage.setItem('dncList', JSON.stringify(dncList)); // Save updated list to localStorage
-        alert(`${phoneNumber} has been added to the DNC list.`);
+
+        // Clear the modal
+        modal.innerHTML = "";
+
+        newP.innerHTML = phoneNumber + " added to DNC list";
+        modal.appendChild(newP);
+        modal.style.display = "block";
     } else {
         document.getElementById("dnc-modal").style.display = "none";
         document.getElementById("enterNumber-modal").style.display = "block";
@@ -22,7 +30,7 @@ function dialNumber() {
             document.getElementById("enterNumber-modal").style.display = "none";
             document.getElementById("dnc-modal").style.display = "block";
         } else {
-            alert("Dialing...");
+            document.getElementById("dialing-modal").style.display = "block";
         }
     } else {
         document.getElementById("dnc-modal").style.display = "none";
@@ -34,13 +42,23 @@ function dialNumber() {
 document.getElementById("dncButton").addEventListener("click", addToDNC);
 document.getElementById("callButton").addEventListener("click", dialNumber);
 
-// Close modal
+// Close modals
 
-document.getElementById("close-button-dnc").addEventListener("click", function() {
+document.getElementById("close-button-dnc").addEventListener("click", function () {
     document.getElementById("dnc-modal").style.display = "none";
 });
 
-document.getElementById("close-button").addEventListener("click", function() {
+document.getElementById("close-button").addEventListener("click", function () {
     document.getElementById("enterNumber-modal").style.display = "none";
 });
+
+document.getElementById("close-dialButton").addEventListener("click", function () {
+    document.getElementById("dialing-modal").style.display = "none";
+});
+
+document.getElementById("close-addDnc").addEventListener("click", function () {
+    document.getElementById("addToDncModal").style.display = "none";
+});
+
+
 
